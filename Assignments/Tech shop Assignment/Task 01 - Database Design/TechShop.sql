3134110a9d0b7e0ddc338db255c748ea9a9b1713
DineshGT <dineshgreet2004@gmail.com>
2025-03-15 08:24:05 +0530

    Task01commit

---
+++ Assignments/Tech shop Assignment/Task 01 - Database Design/TechShop.sql
@@ -0,0 +1,134 @@
+CREATE database TechShop;
+
+use TechShop;
+
+-- the database contains customers, products, orders, orderdetails, inventory.. so first creating all tables
+
+create table Customers
+(
+	cust_id INT PRIMARY KEY,
+	first_name VARCHAR(30) not null, -- no null values
+	last_name VARCHAR(30),
+	cust_email VARCHAR(40) unique,
+	cust_phone NUMERIC(10),
+	cust_address VARCHAR(50) 
+
+);
+
+create table Products
+(
+	prod_id INT PRIMARY KEY,
+	prod_name VARCHAR(30) not null,
+	prod_description VARCHAR(50),
+	prod_price NUMERIC(8)
+
+);
+
+
+create table orders
+(
+	order_id INT PRIMARY KEY,
+	cust_id INT,
+	order_date DATE,
+	total_amnt NUMERIC(8),
+	FOREIGN KEY (cust_id) references Customers(cust_id)
+);
+
+create table order_details
+(
+	order_detail_id INT PRIMARY KEY,
+	order_id INT,
+	prod_id INT,
+	quantity INT,
+	FOREIGN KEY (order_id) references Orders(order_id),
+	FOREIGN KEY (prod_id) references Products(prod_id)
+
+);
+
+
+create table Inventory(
+	inventory_id INT PRIMARY KEY,
+	prod_id INT,
+	quantityInStock INT,
+	lastStockUpdate DATE,
+	FOREIGN KEY (prod_id) references Products(prod_id)
+
+);
+
+-- inserting values to the created tables in techshop
+
+insert into Customers
+(cust_id, first_name, last_name, cust_email, cust_phone, cust_address)
+Values
+(100, 'Arun', 'Prasanth', 'arunprasanth@gmail', 32312, 'abcde'),
+(101, 'Bala', 'Murugan', 'balamurugan@gmail', 42311, 'bcdef'),  
+(102, 'Chitra', 'Devi', 'chitradevi@gmail', 52310, 'cdefg'),  
+(103, 'Dinesh', 'Kumar', 'dineshkumar@gmail', 62309, 'defgh'),  
+(104, 'Eshwar', 'Raj', 'eshwarraj@gmail', 72308, 'efghi'),  
+(105, 'Fathima', 'Zahra', 'fathimazahra@gmail', 82307, 'fghij'),  
+(106, 'Ganesh', 'Babu', 'ganeshbabu@gmail', 92306, 'ghijk'),  
+(107, 'Harini', 'Priya', 'harinipriya@gmail', 102305, 'hijkl'),  
+(108, 'Ilayaraja', 'Perumal', 'ilayaraja@gmail', 112304, 'ijklm'),  
+(109, 'Jayanth', 'Varma', 'jayanthvarma@gmail', 122303, 'jklmn');  
+
+
+insert INTO Products 
+(prod_id, prod_name, prod_description, prod_price) 
+VALUES
+(1, 'Laptop', 'High-performance laptop', 7500),
+(2, 'Smartphone', '5G-enabled smartphone', 45000),
+(3, 'Wireless Earbuds', 'Noise-canceling earbuds', 8500),
+(4, 'Smartwatch', 'Fitness tracking smartwatch', 1200),
+(5, 'Tablet', '10-inch screen tablet', 2800),
+(6, 'Gaming Mouse', 'Ergonomic gaming mouse', 3500),
+(7, 'Mechanical Keyboard', 'RGB backlit keyboard', 6500),
+(8, 'External Hard Drive', '1TB portable storage', 5500),
+(9, 'Monitor', '27-inch Full HD monitor', 18500),
+(10, 'Bluetooth Speaker', 'Waterproof portable speaker', 4000);
+
+
+insert INTO Orders (order_id, cust_id, order_date, total_amnt) 
+values
+(1, 100, '2025-03-10', 120000.00),
+(2, 101, '2025-03-11', 45000.00),
+(3, 102, '2025-03-12', 18500.00),
+(4, 103, '2025-03-13', 75000.00),
+(5, 104, '2025-03-14', 8500.00),
+(6, 105, '2025-03-15', 6500.00),
+(7, 106, '2025-03-16', 3500.00),
+(8, 107, '2025-03-17', 5500.00),
+(9, 108, '2025-03-18', 28000.00),
+(10,109, '2025-03-19', 4000.00);
+
+
+
+insert into Order_Details 
+(order_detail_id, order_id, prod_id, quantity) 
+VALUES
+(11, 1, 1, 2),
+(12, 2, 2, 1),
+(13, 3, 9, 1),
+(14, 4, 1, 1),
+(15, 5, 3, 1),
+(16, 6, 7, 1),
+(17, 7, 6, 1),
+(18, 8, 8, 1),
+(19, 9, 5, 1),
+(20, 10, 10, 1);
+
+
+insert INTO Inventory 
+(inventory_id, prod_id, quantityInStock, lastStockUpdate) 
+VALUES
+(21, 1, 50, '2025-03-01'),
+(22, 2, 40, '2025-03-02'),
+(23, 3, 100, '2025-03-03'),
+(24, 4, 30, '2025-03-04'),
+(25, 5, 25, '2025-03-05'),
+(26, 6, 70, '2025-03-06'),
+(27, 7, 60, '2025-03-07'),
+(28, 8, 45, '2025-03-08'),
+(29, 9, 20, '2025-03-09'),
+(30, 10, 35, '2025-03-10');
+
+
